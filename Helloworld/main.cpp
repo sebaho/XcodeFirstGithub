7206e0651111000e558534729e62742b494b86c9
sebaho <sebaho2002@gmail.com>
2025-01-17 17:59:27 -0500

    Helloworld

--- Helloworld/main.cpp
+++ Helloworld/main.cpp
@@ -9,6 +9,44 @@
 using namespace std;
 
 int main() {
-    cout << 2 +2 << "\n ";
-    return 0 ;
+    cout << "Comments" << "\n";
+    
+// Next Section(VARIABLES)
+    
+    int myNum = 15;
+    double myFloatNum = 5.99;
+    char myLetter = 'D';
+    string myText = "Hello";
+    bool myBoolean = true;
+
+    cout << myNum << myFloatNum << myLetter << myText << "\n";
+
+// Next Section(CONSTANTS)
+    const int myNumber = 15; // with the "const" the variable will remain unchangeable.
+
+// Calculate the are of the rectangle
+    
+    int length = 5;
+    int width = 6;
+    int area;
+    
+area = length * width;
+    
+    cout << "Length is: " << length << "\n";
+    cout << "Width is: " << width << "\n";
+    cout << "Area of the rectangle is: " << area << "\n";
+
+// User Input(CIN >> )
+    
+    int x;
+    cout << "Typea number: ";
+    cin >> x;
+    cout << "Your number is: " << x << "\n";
+    
+// 
+    return 0;
 }
+//
+// Multiline comments start with /* and ends with */
+
+

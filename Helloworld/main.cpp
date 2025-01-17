9fce0359999ffdd7663bd31e387de3bd8785c85a
sebaho <sebaho2002@gmail.com>
2025-01-17 11:58:31 -0500

    Initial Commit

---
+++ Helloworld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Helloworld
+//
+//  Created by sebas ho on 1/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
